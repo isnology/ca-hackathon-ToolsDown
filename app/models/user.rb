6b58e8ee74ce223684b3f2dd061f37c85bec280d
Shaun <soo.shaun@gmail.com>
2017-10-20 10:58:30 +1100

    Resolve schema conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-end
+  
+  has_one :profile
+
+  end
