23ed654f7ae89f0753b8d1d16ff9c026d36b056e
Andrew Mulhall <andrewmulhall@Andrews-MacBook-Pro.local>
2017-10-20 14:29:20 +1100

    welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
 
-
+root  "profiles#index"
   get 'welcome/index'
   resources :welcome
   resources :profiles
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  to: "home#index"
+  
 end
